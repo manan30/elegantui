849982dc38ce68426e3b3a273b335edddd273398
Manan Joshi <mananjoshi1995@gmail.com>
2022-08-07 16:16:22 -0500

    feat: bootstrapping form package

---
+++ packages/form/README.md
@@ -0,0 +1,13 @@
+# @elegantui/form
+
+ElegantUI form components
+
+## Installation
+
+```bash
+npm i @elegantui/form
+```
+
+## License
+
+MIT © [Manan Joshi](https://mananjoshi.me)
---
+++ packages/form/tailwind.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const baseTailwindConfig = require('../../tailwind-base.config');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ...baseTailwindConfig,
+  content: ['./**/*.tsx']
+};
