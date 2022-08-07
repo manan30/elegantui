acf695d2fdc4aee79641f5b2061062f7de5e5ff5
Manan Joshi <mananjoshi1995@gmail.com>
2022-08-07 01:14:55 -0500

    feat: exporting the badge component

--- packages/atoms/index.ts
+++ packages/atoms/index.ts
@@ -1,5 +1,6 @@
 import Button from './button';
 import Dialog from './dialog';
 import Card from './card';
+import Badge from './badge';
 
-export { Button, Dialog, Card };
+export { Button, Dialog, Card, Badge };
