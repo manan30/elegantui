bc872e755ab84180333a46f104013ea4cf326905
Manan Joshi <mananjoshi1995@gmail.com>
2022-08-10 00:11:10 -0500

    feat: adding select component to forms package

--- packages/forms/index.ts
+++ packages/forms/index.ts
@@ -1,5 +1,6 @@
 import Input from './input';
 import Checkbox from './checkbox';
 import RadioButton from './radio-button';
+import Select from './select';
 
-export { Input, Checkbox, RadioButton };
+export { Input, Checkbox, RadioButton, Select };
