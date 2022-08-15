520f3a803fdad8a4bd1689fd7fb08407a2909d0c
Manan Joshi <mananjoshi1995@gmail.com>
2022-08-15 01:21:42 -0500

    docs: adding introduction and getting started pages

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,7 +1,8 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
-import 'nextra-theme-docs/style.css';
+// TODO: Add new tailwind classes build
 import '@elegantui/forms/dist/styles.css';
+import 'nextra-theme-docs/style.css';
 
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />;
