cc0401afa4c6aec4d2897428505df782b0212e8b
Manan Joshi <mananjoshi1995@gmail.com>
2022-08-16 22:58:20 -0500

    docs: documenting all props for the dialog component

--- docs/components/Dialog.tsx
+++ docs/components/Dialog.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Button, Dialog } from '@elegantui/atoms';
 
-const DialogComponent = () => {
+const DialogComponent = ({
+  title,
+  disableCloseOnEsc,
+  disableCloseButton,
+  size
+}: {
+  title?: string;
+  disableCloseOnEsc?: boolean;
+  disableCloseButton?: boolean;
+  size?: 'sm' | 'md' | 'lg' | 'xl';
+}) => {
   const [open, setOpen] = React.useState(false);
   return (
     <>
-      <Button style={{ marginTop: '1rem' }} onClick={() => setOpen(true)}>
-        Open Dialog
+      <Button
+        style={{ marginTop: '1rem', marginRight: '0.5rem' }}
+        onClick={() => setOpen(true)}
+      >
+        {title && title !== 'My Dialog' ? title : 'Open Dialog'}
       </Button>
-      <Dialog open={open} dismissHandler={() => setOpen(false)}>
+      <Dialog
+        open={open}
+        dismissHandler={() => setOpen(false)}
+        title={title}
+        disableCloseOnEsc={disableCloseOnEsc}
+        disableCloseButton={disableCloseButton}
+        size={size}
+      >
         <h2
           style={{
             marginTop: 0,
