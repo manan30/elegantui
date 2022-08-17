8413c43df806e9cb48201416987e71daadd3a8dd
Manan Joshi <mananjoshi1995@gmail.com>
2022-08-17 14:30:51 -0500

    docs: adding tracking script

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,9 +1,22 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
+import NextScript from 'next/script';
+
 import '@elegantui/atoms/dist/styles.css';
 import 'nextra-theme-docs/style.css';
 import '../styles/global.css';
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {process.env.NODE_ENV === 'production' ? (
+        <NextScript
+          data-website-id='44ef75be-3be4-4e59-9107-dff0ebe6c234'
+          src='https://wave-studios-tracking.up.railway.app/umami.js'
+          strategy='lazyOnload'
+        />
+      ) : null}
+      <Component {...pageProps} />
+    </>
+  );
 }
