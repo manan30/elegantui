31be7d42b91c9ebf28006a70e41aeca2d921993f
Manan Joshi <mananjoshi1995@gmail.com>
2022-08-20 01:02:19 -0500

    feat: making common vitebook for both packages

--- packages/atoms/.vitebook/.temp/dccab16d941be916795c93908057691cf6d27025.mjs
+++ packages/atoms/.vitebook/.temp/dccab16d941be916795c93908057691cf6d27025.mjs
@@ -4,30 +4,28 @@ import __vitebook__path from 'path';
 import { fileURLToPath as __vitebook__fileURLToPath } from 'url';
 import { createRequire as __vitebook__createRequire } from 'module';
 const require = __vitebook__createRequire(import.meta.url);
-var __require = function(x) { return require(x); };
+var __require = function (x) {
+  return require(x);
+};
 __require.__proto__.resolve = require.resolve;
 const __filename = __vitebook__fileURLToPath(import.meta.url);
 const __dirname = __vitebook__path.dirname(__filename);
 
 // .vitebook/config.ts
-import { clientPlugin, defineConfig } from "@vitebook/client/node";
-import { preactPlugin } from "@vitebook/preact/node";
-import {
-  defaultThemePlugin
-} from "@vitebook/theme-default/node";
+import { clientPlugin, defineConfig } from '@vitebook/client/node';
+import { preactPlugin } from '@vitebook/preact/node';
+import { defaultThemePlugin } from '@vitebook/theme-default/node';
 var config_default = defineConfig({
-  include: ["**/*.story.tsx"],
+  include: ['**/*.story.tsx'],
   plugins: [
-    preactPlugin({ appFile: "App.tsx" }),
+    preactPlugin({ appFile: 'App.tsx' }),
     clientPlugin(),
     defaultThemePlugin()
   ],
   site: {
-    title: "ElegantUI Atoms",
-    description: "ElegantUI base components",
+    title: 'ElegantUI Atoms',
+    description: 'ElegantUI base components',
     theme: {}
   }
 });
-export {
-  config_default as default
-};
+export { config_default as default };
---
+++ tailwind.vitebook.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const baseTailwindConfig = require('./tailwind-base.config');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ...baseTailwindConfig,
+  darkMode: 'class',
+  content: [
+    './packages/**/*.tsx',
+    './packages/**/*.story.tsx',
+    './.vitebook/App.tsx'
+  ],
+  plugins: []
+};
